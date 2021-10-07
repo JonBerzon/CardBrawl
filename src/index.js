82dffe7fd387a738f83d0b5dbae11ff653c32c09
Jonathan Berzon <jonathanlberzon@gmail.com>
2021-10-07 17:10:23 -0400

    added structural HTML and flexed with css

---
+++ src/index.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const canvas = document.getElementById("game-canvas");
+    const ctx = canvas.getContext('2d')
+
+    canvas.width = 1237; //1237 - 1650
+    canvas.height = 750; // 750 - 1000
+   
+});
