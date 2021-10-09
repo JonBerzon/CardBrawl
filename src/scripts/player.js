3c706105da3908b55b92eb0ee19d8c437e705b6e
Jonathan Berzon <jonathanlberzon@gmail.com>
2021-10-09 16:47:29 -0400

    user greyscaling fixed and works

--- src/scripts/player.js
+++ src/scripts/player.js
@@ -10,8 +10,12 @@ export class Player{
         if (!this.useMana(mana)){
             alert("You used too much mana, try again.")
         } else {
+            this.greyMana();
             this.addShield(shield)
+            this.updateShields();
             this.takeDamage(damage)
+            this.updateShields();
+            this.greyHealth();
         }
         
         
@@ -20,37 +24,71 @@ export class Player{
     fullReset(){
         this.reset();
         this.hp = 10;
+        this.nonGreyHealth();
+
 
     }
+    nonGreyShields(){
+        for (let i = 1; i < this.shield + 1; i++) {
+            document.getElementById(`shield-${i}`).style.removeProperty('filter')
+        }
+    }
+    
 
-    greyShields(){
-        for (let i = 1; i < 5; i++) {
+    updateShields(){
+        for (let i = 4; this.shield < i ; i--) {
             document.getElementById(`shield-${i}`).style.filter = "grayscale(1)"
+        }
+        for (let j = 1; j < this.shield + 1; j++) {
+            document.getElementById(`shield-${j}`).style.removeProperty('filter')
+            
+        }
+    }
+
+    greyMana(){
+        for (let i = 1; i < 3 - this.mana + 1; i++) {
+            document.getElementById(`mana-${i}`).style.filter = "grayscale(1)"
+        }
+    }
+
+    nonGreyMana(){
+        for (let i = 1; i < 4; i++) {
+            document.getElementById(`mana-${i}`).style.removeProperty('filter')
+        }
+    }
+    greyHealth(){
+        for (let i = 1; i < 10 - this.hp + 1; i++) {
+            document.getElementById(`player-heart-${i}`).style.filter = "grayscale(1)";   
+        }
+    }
+
+    nonGreyHealth(){
+        for (let i = 1; i < 11; i++) {
+            document.getElementById(`player-heart-${i}`).style.removeProperty('filter')
         }
     }
 
     reset(){
         this.mana = 3;
         this.shield = 0;
-        this.greyShields();
+        this.updateShields();
+        this.nonGreyMana();
         
     }
 
     addShield(num){
-        // let test = document.getElementById("shieldTest")
-        // test.style.filter = "grayscale(1)"
-        // test.style.addProperty()
-        // console.log(test)
-        this.shield += num;
-        for (let i = 1; i < this.shield + 1; i++) {
-            document.getElementById(`shield-${i}`).style.removeProperty('filter')
-        }
+        this.shield += num;  
     }
 
     takeDamage(num){
         let combinedHealth = this.shield + this.hp;
         combinedHealth -= num;
-        if (this.hp > combinedHealth) this.hp = combinedHealth;
+        if (this.hp >= combinedHealth) {
+            this.hp = combinedHealth;
+            this.shield = 0;
+        } else {
+            this.shield -= num;
+        }
         if (!this.isAlive()) alert("You died")
     }
 
